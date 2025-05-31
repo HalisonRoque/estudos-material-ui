4350606cdf84a546ce537ba7d843eee9e63eb736
halisonyasha@gmail.com <halisonyasha@gmail.com>
2025-05-30 23:21:20 -0300

    configurando o react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,24 @@
-export {  };
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+    return (
+        /*ROTAS DO PROJETO*/
+        <Routes>
+            {/*ROTAS 1*/}
+            <Route
+                path="/pagina-inicial"
+                element={
+                    <p>Página inicial aqui</p>
+                }
+            />
+
+            {/*ROTAS DEFAULT*/}
+            <Route
+                path="*"
+                element={
+                    <Navigate to="/pagina-inicial" />
+                }
+            />
+        </Routes>
+    );
+};
